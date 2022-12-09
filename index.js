50d8bc464c5517e9acb37509423cf06cb395adcd
Uday Kumar <dudaykumargoud@gmail.com>
2022-12-09 20:23:37 +0530

    feat : getting data fro urls

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+const express = require("express");
+const app = express();
+const axios = require("axios");
+const controller = async (req, res) => {
+  const urls = req.query.url;
+  //   const urlsArray = urls.split(",");
+  try {
+    const responseArray = [];
+    for (let i = 0; i < urls.length; i++) {
+      console.log(urls[i]);
+      const response = await axios.post(urls[i]);
+      responseArray.push(response.data.numbers);
+    }
+    console.log(responseArray);
+  } catch (error) {
+    console.log(error);
+  }
+};
+app.all("/", controller);
+app.listen(process.env.PORT || 3000);
